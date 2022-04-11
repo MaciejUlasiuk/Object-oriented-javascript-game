727eba44383ed825079b4528a5c3eb4f269e104e
MaciejUlasiuk <filip123xd@o2.pl>
2022-04-11 23:11:50 +0200

    keyboard spaceship control

--- js/Spaceship.js
+++ js/Spaceship.js
@@ -21,6 +21,12 @@
         this.spaceship.style.left = `${(e.clientX - (this.spaceship.offsetWidth/2) )}px`;
         this.spaceship.style.top = `${(e.clientY - (this.spaceship.offsetHeight/2))}px`;
      }
+     keyboardMoveLeft = () => {
+         this.spaceship.style.left = `${this.spaceship.offsetLeft - 1} px`
+     }
+     keyboardMoveRight = () => {
+        this.spaceship.style.left = `${this.spaceship.offsetLeft + 1} px`
+    }
      toggleSpaceshipAnimation(){
          this.spaceship.style.animationPlayState = 'running' ? 'paused' : 'running'
      }
