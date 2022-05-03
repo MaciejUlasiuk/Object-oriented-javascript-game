3f2cb7ef2083ca51e5e627deee4dbe53d11aa56a
MaciejUlasiuk <filip123xd@o2.pl>
2022-05-03 22:05:59 +0200

    add

---
+++ js/Missiles.js
@@ -0,0 +1,27 @@
+export class Missiles {
+
+
+    normalMissiles = [];
+    enemyMissiles = [];
+
+
+    checkEnemyMissilePosition = () =>
+    {
+        this.enemyMissiles.forEach((missile, index) => {
+            const missilePosition = {
+                top: missile.missile.offsetTop,
+                right: missile.missile.offsetLeft + missile.missile.offsetWidth,
+                bottom: missile.missile.offsetTop + missile.missile.offsetHeight,
+                left: missile.missile.offsetLeft
+            }
+           if(missilePosition.top<=0 || missilePosition.top>=window.innerHeight)
+           {
+               
+               missile.missile.remove()
+               this.enemyMissiles.splice(index,1)
+               clearInterval(missile.interval)
+               console.log('yo  ')
+           }
+        }
+        )}
+ }
